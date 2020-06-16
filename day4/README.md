d1597a68b38b0a3b24604ff3e6408ab6884c52d2
csh8130 <csh8130@naver.com>
2020-06-16 23:39:43 +0900

    day4 init

---
+++ day4/README.md
@@ -0,0 +1,7 @@
+## 100DaysOfSwift - Day4
+### Loops
+### 반복문
+
+> 매일 1시간 Swift 100일 - 4일 째
+> 
+> https://www.hackingwithswift.com/100/4
---
+++ day4/practice.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+/*:
+ ### [100days of Swift - day4](https://www.hackingwithswift.com/100/3)
+ ## 반복문 (루프)
+ */
+//: ### [1. For loops](https://www.hackingwithswift.com/sixty/4/1/for-loops)
+//: ### [1.1 Swfit는 왜 반복문에 _ 밑줄을 사용해야 할까?](https://www.hackingwithswift.com/quick-start/understanding-swift/why-does-swift-use-underscores-with-loops)
+
+//: ### [2. While loops](https://www.hackingwithswift.com/sixty/4/2/while-loops)
+//: ### [2.1 언제 while loop를 사용하는가?](https://www.hackingwithswift.com/quick-start/understanding-swift/when-should-you-use-a-while-loop)
+
+//: ### [3. Repeat loops](https://www.hackingwithswift.com/sixty/4/3/repeat-loops)
+//: ### [3.1 언제 repeat loop를 사용하는가?](https://www.hackingwithswift.com/quick-start/understanding-swift/when-should-you-use-a-repeat-loop)
+
+//: ### [4. Exiting loops](https://www.hackingwithswift.com/sixty/4/4/exiting-loops)
+//: ### [4.1 루프를 종료해야하는 상황](https://www.hackingwithswift.com/quick-start/understanding-swift/why-would-you-want-to-exit-a-loop)
+
+//: ### [5. Exiting multiple loops](https://www.hackingwithswift.com/sixty/4/5/exiting-multiple-loops)
+//: ### [5.1 루프에 라벨을 붙이는이유](https://www.hackingwithswift.com/quick-start/understanding-swift/why-does-swift-have-labeled-statements)
+
+//: ### [6. Skipping items](https://www.hackingwithswift.com/sixty/4/6/skipping-items)
+//: ### [6.1 break, continue 사용 시기](https://www.hackingwithswift.com/quick-start/understanding-swift/when-to-use-break-and-when-to-use-continue)
+
+//: ### [7. 무한루프](https://www.hackingwithswift.com/sixty/4/7/infinite-loops)
+//: ### [7.1 무한루프를 만들고 싶은 상황](https://www.hackingwithswift.com/quick-start/understanding-swift/why-would-you-want-to-make-an-infinite-loop)
+
+//: ### [루프 요약 ](https://www.hackingwithswift.com/sixty/4/8/looping-summary)
